bcdfa9851a534fb4ca3f22a6d2ff9fce4daf7e36
Hugo Manrique <hugmanrique@gmail.com>
2019-06-16 17:22:04 +0200

    Initial commit
    Includes the logger lib and the webpack base.

---
+++ packages/logger/src/index.js
@@ -0,0 +1,21 @@
+import { bold, cyan, yellow, redBright } from 'colorette';
+
+const prefixes = {
+  info: bold(cyan('INFO')),
+  warn: bold(yellow('WARN')),
+  error: bold(redBright('ERROR'))
+};
+
+/**
+ * Prints a nicely formatted message to the console.
+ *
+ * @param {*} prefix the log entry prefix
+ * @param {*} message the log entry message
+ */
+function printMessage(prefix, message) {
+  console.log(`Lyra ${prefix} ${message}`);
+}
+
+export const info = message => printMessage(prefixes.info, message);
+export const warn = message => printMessage(prefixes.warn, message);
+export const error = message => printMessage(prefixes.error, message);
---
+++ packages/webpack-config/src/index.js
@@ -0,0 +1,3 @@
+import logger from '@lyra/logger';
+
+logger.info('foo bar');
