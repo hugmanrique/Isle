5acfb7311c8115bdeeb74df23edcc25eeccf7312
Hugo Manrique <hugmanrique@gmail.com>
2019-06-16 18:22:17 +0200

    Add dev server config

--- packages/webpack-config/src/devServer.js
+++ packages/webpack-config/src/devServer.js
@@ -1,42 +1,30 @@
 /**
  * Creates a `devServer` webpack config section in development
- * mode, returns `null` otherwise.
+ * mode, returns an empty object otherwise.
  *
  * @param {Object} config - devServer settings
  * @param {Boolean} config.isProduction - whether Lyra is running in production mode
- * @param {String} config.publicPath - determines where bundles should be served from.
  * @param {String} config.contentBase - path to serve static resources from
+ * @param {String} config.publicPath - determines where bundles should be served from.
  * @returns {Object} a `devServer` webpack config section
  * @see {@link https://webpack.js.org/configuration/dev-server/}
  */
-export default function createDevServerConfig({ isProduction, publicPath }) {
-  return;
-}
+export default function createDevServerConfig({
+  isProduction,
+  contentBase,
+  publicPath
+}) {
+  if (isProduction) {
+    return {};
+  }
 
-/**
- * Creates an array of webpack `module.rules` according
- * to the current settings such as the environment.
- *
- * @param {Object} config - rules settings
- * @param {Boolean} config.isProduction - whether Lyra is running in production mode
- * @returns {Object[]} an array of webpack module rules
- */
-function createModuleRules({ isProduction }) {
-  return [
-    {
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      use: [
-        'thread-loader',
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@lyra/babel-preset'],
-            cacheDirectory: true,
-            cacheCompression: isProduction
-          }
-        }
-      ]
-    }
-  ];
+  return {
+    contentBase,
+    // File updates from `contentBase` will trigger a page reload.
+    watchContentBase: true,
+    // Enable hot reloading server. Currently only changes to CSS are hot
+    // reloaded. JS changes will refresh the browser.
+    hot: true,
+    publicPath
+  };
 }
