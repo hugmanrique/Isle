24354df00e53535513ae8e9ce807f3a8c9bc3aef
Hugo Manrique <hugmanrique@gmail.com>
2019-06-16 17:38:06 +0200

    Create Lyra babel-preset

---
+++ packages/babel/index.js
@@ -0,0 +1,26 @@
+module.exports = {
+  presets: [
+    // Transpile ES6+ code
+    '@babel/preset-env',
+    // Transpile JSX
+    '@babel/preset-react'
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-syntax-dynamic-import'
+  ],
+  env: {
+    production: {
+      plugins: [
+        // Remove prop-types in production
+        [
+          'transform-react-remove-prop-types',
+          {
+            removeImport: true
+          }
+        ]
+      ]
+    }
+  }
+};
