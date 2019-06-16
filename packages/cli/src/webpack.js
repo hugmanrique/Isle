957784c44b075fa96457c09842669edb9734a3d8
Hugo Manrique <hugmanrique@gmail.com>
2019-06-16 23:26:34 +0200

    Document functions

--- packages/cli/src/webpack.js
+++ packages/cli/src/webpack.js
@@ -2,12 +2,24 @@ import webpack from 'webpack';
 import { __internalGetWebpackConfig } from '@isle/webpack-config';
 import logger from '@isle/logger';
 
+/**
+ * Creates a webpack `Compiler` instance with Isle's config
+ * grabbed from `@isle/webpack-config`.
+ *
+ * @returns {Object} a webpack `Compiler` instance
+ */
 function createCompiler() {
   const config = __internalGetWebpackConfig();
 
   return webpack(config);
 }
 
+/**
+ * webpack compile callback that formats log messages.
+ *
+ * @param {Error} err - an optional compilation error
+ * @param {Object} stats - webpack's stat object
+ */
 function onCompile(err, stats) {
   if (err) {
     logger.error(err);
@@ -31,12 +43,18 @@ function onCompile(err, stats) {
   }
 }
 
+/**
+ * Starts a webpack compilation.
+ */
 export function startBuild() {
   const compiler = createCompiler();
 
   compiler.run(onCompile);
 }
 
+/**
+ * Starts a webpack watching session.
+ */
 export function startWatching() {
   const compiler = createCompiler();
 
