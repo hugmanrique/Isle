f55c4f76451f0e456c66b489af0e32a05936c623
Hugo Manrique <hugmanrique@gmail.com>
2019-06-18 12:45:40 +0200

    Add TODO about react-snap

--- packages/cli/src/prerender.js
+++ packages/cli/src/prerender.js
@@ -1,6 +1,7 @@
 import { run as prerender } from 'react-snap';
 
 export default function startPrerender({ source }) {
+  // TODO Add end user config to setup react-snap
   const config = {
     source,
     puppeteerArgs: ['--no-sandbox']
