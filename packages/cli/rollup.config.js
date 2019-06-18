2986691fb834531125f2d4c6ed62470fa895038a
Hugo Manrique <hugmanrique@gmail.com>
2019-06-18 19:58:50 +0200

    Fix shebang

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,16 +1,15 @@
-import banner from 'rollup-plugin-banner';
+import shebang from 'rollup-plugin-preserve-shebang';
 
 import defaultConfig from '../../default.rollup.config';
 import pkg from './package.json';
 
 const config = defaultConfig(pkg);
 
-// Workaround until https://github.com/yingye/rollup-plugin-banner/issues/1 gets fixed
-config.output.sourcemap = false;
-
 config.plugins.push(
   // Declare shebang
-  banner('#/usr/bin/env node')
+  shebang({
+    shebang: '#!/usr/bin/env node'
+  })
 );
 
 export default config;
