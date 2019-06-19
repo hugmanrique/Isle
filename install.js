c1b8479986272df8c1145e011e812a8ae9b82326
Hugo Manrique <hugmanrique@gmail.com>
2019-06-19 21:19:06 +0200

    Add fix for npm install missing trans-deps

---
+++ install.js
@@ -0,0 +1,90 @@
+/**
+ * npm likes to close perfectly valid issues. This is a bruteforce hack
+ * that installs transitive dependencies of local dependencies.
+ *
+ * This fix will waste disk space (duplicate deps),
+ * feel free to contribute a better fix :)
+ *
+ * https://npm.community/t/npm-install-does-not-install-transitive-dependencies-of-local-dependency/2264
+ * https://github.com/npm/npm/issues/13734
+ * https://github.com/npm/npm/issues/20835
+ */
+
+const fs = require('fs');
+const path = require('path');
+const { exec, spawn } = require('child_process');
+
+const exampleAppDir = path.resolve(__dirname, 'example');
+const modules = fs.readdirSync('packages');
+const npmOptions = { cwd: exampleAppDir };
+
+const packageDirs = {};
+const appDependencies = [];
+
+const addDependencies = (deps = {}) =>
+  appDependencies.push(...Object.keys(deps));
+
+function parseDependencies() {
+  modules.forEach(dirName => {
+    const packageJsonPath = path.join('packages', dirName, 'package.json');
+
+    const {
+      name,
+      dependencies,
+      peerDependencies
+    } = require(`./${packageJsonPath}`);
+
+    packageDirs[name] = dirName;
+
+    // Install everything just to be safe
+    addDependencies(dependencies);
+    addDependencies(peerDependencies);
+  });
+}
+
+const logHandler = data => console.log(data.toString());
+
+function fixLocalDeps() {
+  for (let i = 0; i < appDependencies.length; i++) {
+    const depName = appDependencies[i];
+
+    if (!depName.startsWith('@isle')) {
+      continue;
+    }
+
+    const dirName = packageDirs[depName];
+
+    if (!dirName) {
+      throw new Error(`Cannot find local package ${depName} in "packages/"`);
+    }
+
+    appDependencies[i] = `../packages/${dirName}`;
+  }
+}
+
+function attachLogger(child) {
+  child.stdout.on('data', logHandler);
+  child.stderr.on('data', logHandler);
+  child.on('error', console.error);
+}
+
+// Run initial install
+const globalInstall = exec('npm i', npmOptions, err => {
+  if (err) {
+    throw err;
+  }
+
+  parseDependencies();
+
+  fixLocalDeps();
+
+  console.log(`Installing ${appDependencies.length} (direct) transitive deps`);
+
+  const fixArgs = ['install', '--no-save'].concat(appDependencies);
+
+  const transInstall = spawn('npm', fixArgs, npmOptions);
+
+  attachLogger(transInstall);
+});
+
+attachLogger(globalInstall);
